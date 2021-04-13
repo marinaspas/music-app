fe6562bbde5cd645ad6cff6bc295790b213a260f
marinaspas <msmarinaspas@gmail.com>
2021-04-13 18:54:32 -0400

    the music app changes so far setting up the music app with components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [songs, setSongs] = useState([
+    {
+      title: "Mona Lisa",
+      artist: "Valntn, Peter Fenn, Tray Haggerty",
+      img_src: "./images/monalisa-bg.jpg",
+      altText: "Mona Lisa - Valntn cover art",
+      src: "./music/VLNTN - Mona Lisa.mp3",
+    },
+    {
+      title: "Day by Day",
+      artist: "Fiji Blue",
+      img_src: "./images/fiji-song-bg.jpg",
+      altText: "Day by Day - Fiji Blue cover art",
+      src: "./music/Fiji Blue - Day by Day.mp3",
+    },
+    {
+      title: "Feel Good",
+      artist: "Syn Cole",
+      img_src: "./images/feelgood-bg.jpg",
+      altText: "Feel Good - Syn cole cover art",
+      src: "./music/Syn Cole - Feel Good.mp3",
+    },
+    {
+      title: "Angel Boy",
+      artist: "William Bolton",
+      img_src: "./images/angel-boy.jpg",
+      altText: "Angel Boy - William Bolton cover art",
+      src: "./music/William Bolton - Angel Boy.mp3",
+    },
+  ]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
